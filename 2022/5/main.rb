1b210219e98abc4d7bb00e1f3944c3fa02b31731
Timo <timo.nock@stud.h-da.de>
2022-12-05 17:20:32 +0100

    you know what, fuck you *un-evals your solution*

--- 2022/5/main.rb
+++ 2022/5/main.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-x, y = File.read('input.txt').split("\n\n").map { |l| l.split("\n") }
+x, y = File.read('input.txt').split("\n\n").map { |i| i.split("\n") }
 stacks = Array.new(x.pop[-2].to_i + 2) { [] }
 
 # Parsing bs
@@ -10,19 +10,10 @@ stacks.reject!(&:empty?)
 original_stacks = stacks.map(&:dup)
 
 # Part 1
-instructions = y.map do |l|
-  i = l.split
-  "stacks[#{i.last.to_i - 1}].unshift(stacks[#{i[3].to_i - 1}].shift(#{i[1]}).reverse)
- stacks[#{i.last.to_i - 1}].flatten!"
-end
-
-instructions.each { |i| eval(i) }
-
+y.each { |i| stacks[i[-1].to_i - 1].unshift(stacks[i[12..13].to_i - 1].shift(i[5..6].to_i).reverse).flatten! }
 p stacks.map(&:shift).join # => MQSHJMWNH
 
-# Part 2
 stacks = original_stacks
-
-instructions.map { |l| l.gsub('.reverse', '') }.each { |i| eval(i) }
-
+# Part 2
+y.each { |i| stacks[i[-1].to_i - 1].unshift(stacks[i[12..13].to_i - 1].shift(i[5..6].to_i)).flatten! }
 p stacks.map(&:shift).join # => LLWJRBHVZ
