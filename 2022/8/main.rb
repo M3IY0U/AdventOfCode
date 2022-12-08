8156202d501e6774f9e3ef2117cd5334690faf1c
Timo <timo.nock@stud.h-da.de>
2022-12-08 12:27:20 +0100

    Add the worst code I've written to date (Day 8)

---
+++ 2022/8/main.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+$grid = File.readlines('input.txt').map { |l| l.chomp.split('').map(&:to_i) }
+
+def check_visibility(x, y)
+  # check row
+  left = (0...x).to_a.all? { |t| $grid[t][y] < $grid[x][y] }
+  right = ((x + 1)...$grid.length).to_a.all? { |t| $grid[t][y] < $grid[x][y] }
+
+  # check column
+  top = (0...y).to_a.all? { |t| $grid[x][t] < $grid[x][y] }
+  bottom = ((y + 1)...$grid[0].length).to_a.all? { |t| $grid[x][t] < $grid[x][y] }
+
+  left || right || top || bottom
+end
+
+trees = []
+(1...($grid.length - 1)).each do |x|
+  (1...($grid[0].length - 1)).each do |y|
+    trees << check_visibility(x, y)
+  end
+end
+
+p trees.count(true) + ($grid.length * 2 + $grid[0].length * 2 - 4) # => 1818
+
+def calc_scenic_score(x, y)
+  current = $grid[x][y]
+  i = x
+  j = y
+  scores = [0, 0, 0, 0]
+  # check row
+  loop do
+    scores[0] += 1
+    break if i - 1 <= 0 || $grid[i - 1][y] >= current
+
+    i -= 1
+  end
+  i = x
+  loop do
+    scores[1] += 1
+    break if i + 1 >= $grid.length || $grid[i + 1][y] >= current
+
+    i += 1
+  end
+
+  # check column
+  loop do
+    scores[2] += 1
+    break if j - 1 <= 0 || $grid[x][j - 1] >= current
+
+    j -= 1
+  end
+
+  j = y + 1 # "why add + 1 here?" 🤓🤓🤓
+  loop do
+    scores[3] += 1
+    break if j + 1 >= $grid[0].length || $grid[x][j + 1] >= current
+
+    j += 1
+  end
+
+  scores
+end
+
+scores = []
+(1...($grid.length - 1)).each do |x|
+  (1...($grid[0].length - 1)).each do |y|
+    scores << calc_scenic_score(x, y)
+  end
+end
+
+p scores.map { |a| a.inject(:*) }.max # => 368368
